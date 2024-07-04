1e148d84ef73229cb06229fef24c7be1fe43a690
caleholst <caleholst@gmail.com>
2024-07-04 13:40:41 -0500

    Added input validation, Updated the name on the admin layout

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,29 @@
-﻿namespace CIS174Final.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIS174Final.Models
 {
     public class Book
     {
         public int BookId { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9,?!\- ""']+$", ErrorMessage = "Title can only contain letters, numbers, comma, hyphen, space, question mark, exclamation mark, double quote, and single quote")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(50, ErrorMessage = "Author can't be longer than 50 characters")]
+        [RegularExpression(@"^[a-zA-Z\-\""]+$", ErrorMessage = "Author can only contain letters, hyphen, double quote, and single quote")]
         public string Author { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Publish year must be after 0")]
         public int PublishYear { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10")]
         public float Rating { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(1000, ErrorMessage = "Description can't be longer than 1000 characters")]
         public string Description { get; set; }
 
         public string Slug => Title?.Replace(' ', '-').ToLower() + '-' + Author?.ToString();
@@ -17,8 +34,11 @@
     public class Review
     {
         public int ReviewId { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5")]
         public int Rating { get; set; }
 
+        [Required]
         public Book Book { get; set; }
     }
 }
