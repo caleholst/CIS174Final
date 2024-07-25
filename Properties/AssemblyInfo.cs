1485479f99200f39e3ca1ecd6167242311ef07d9
caleholst <caleholst@gmail.com>
2024-07-25 14:57:17 -0500

    Added Unit Test File

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("YourCompany")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("CIS174Final")]
-[assembly: AssemblyTitle("CIS174Final")]
-[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyCompany("YourCompany")]
+//[assembly: AssemblyConfiguration("Release")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyInformationalVersion("1.0.0")]
+//[assembly: AssemblyProduct("CIS174Final")]
+//[assembly: AssemblyTitle("CIS174Final")]
+//[assembly: AssemblyVersion("1.0.0.0")]
